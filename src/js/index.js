f774ff3fb63af80f78a2e31cf5a821b94d2ae55f
David <ash191919191919@gmail.com>
2020-05-18 23:55:23 +0300

    Class for hotel and start to make calendar

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ import './jquery-ui.min.js';
 // import './includes/radio_btn';
 // import './includes/toggle';
 // import './includes/like_btn';
-// import './includes/rate_btn';
+import './card';
+import './includes/rate_btn';
 // import './includes/slider';
 // import './includes/pagination';
