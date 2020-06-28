04cdad2b685e7be916ecd4811b64edc30b879eeb
David <ash191919191919@gmail.com>
2020-06-28 23:04:16 +0300

    дорабатываю календарь, добавил кнопку очистить и применить

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'jquery';
-import './js/index';
 import './assets/css/main.css';
 import './assets/sass/main.sass';
+import './js/index';
 
--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,3 +1,26 @@
-$.fn.datepicker = {
-    inline: true
-};
+let datepickers_container = document.querySelector('.datepickers-container');
+// console.log(call)
+
+
+$("#date_1").datepicker({
+    clearButton: true,
+    range: true,
+    // todayButton: true,
+    // inline: true
+});
+$("#date_2").datepicker({
+    clearButton: true,
+    range: true,
+    // inline: true
+});
+
+document.querySelector('.calendar__btn').innerHTML = `<div class = 'calendar__btn'>
+                    <div class = 'calendar__clear clean'>Очистить</div>
+                    <div class = 'calendar__apply'>Применить</div>
+        </div>`;
+document.querySelector('.calendar__apply').style.position = 'absolute';
+document.querySelector('.calendar__apply').style.left = '100px';
+// let clean = document.getElementsByClassName('clean')[0];
+// clean.addEventListener('click', (e) => {
+//     myDatepicker.clear();
+// })
