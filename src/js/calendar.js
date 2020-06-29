375dc8e5cc8aa08139013825414f6db300ba3efe
David <ash191919191919@gmail.com>
2020-06-29 23:13:56 +0300

    доделал календарь

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,26 +1,51 @@
-let datepickers_container = document.querySelector('.datepickers-container');
-// console.log(call)
+
+let date1 = $("#date_1").datepicker().data('datepicker');
+
+let date1Input = document.getElementById('date_1');
+let date2Input = document.getElementById('date_2');
+
+
+
 
 
 $("#date_1").datepicker({
     clearButton: true,
     range: true,
-    // todayButton: true,
-    // inline: true
-});
-$("#date_2").datepicker({
-    clearButton: true,
-    range: true,
-    // inline: true
+    // inline: true,
+    onSelect: function(formattedDate,date,inst) {
+        date1Input.value = formattedDate.split(',')[0]
+        if(formattedDate.split(',')[1] != undefined) {
+            date2Input.value = formattedDate.split(',')[1]
+        }
+    },
+    toggleSelected: false,
+    
 });
 
-document.querySelector('.calendar__btn').innerHTML = `<div class = 'calendar__btn'>
-                    <div class = 'calendar__clear clean'>Очистить</div>
-                    <div class = 'calendar__apply'>Применить</div>
-        </div>`;
-document.querySelector('.calendar__apply').style.position = 'absolute';
-document.querySelector('.calendar__apply').style.left = '100px';
-// let clean = document.getElementsByClassName('clean')[0];
-// clean.addEventListener('click', (e) => {
-//     myDatepicker.clear();
-// })
+
+
+document.querySelectorAll('.calendar__btn')[0].innerHTML = `<div class = 'calendar__clear'>Очистить</div>
+                                                            <div class = 'calendar__apply'>Применить</div>`;
+
+
+
+
+
+
+
+
+
+
+let clean = document.getElementsByClassName('calendar__clear')[0];
+
+clean.addEventListener('click', () => {
+    date1.clear();
+    date1Input.value = '';
+    date2Input.value = '';
+});
+
+
+document.querySelector('.calendar__apply').addEventListener('click', () => {
+    date1.hide();
+});
+
